b2ce77421e2b03a44f64b897f5315968971b476d
Cristian <crifergon1@gmail.com>
2022-07-07 00:12:32 +0200

    level 1 finished!

--- src/void.js
+++ src/void.js
@@ -9,13 +9,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 const APIJOKE = 'https://icanhazdadjoke.com';
+const jokes = [];
 function letsJoke() {
     return __awaiter(this, void 0, void 0, function* () {
-        //const joke : string = await makeJoke().then(req => req.joke);
         const joke = yield makeJoke().then(req => req);
         const stringJoke = joke.joke;
         const textUser = document.getElementById("joke");
         textUser.innerHTML = stringJoke;
+        pushJoke(joke);
+        console.log(jokes);
     });
 }
 function makeJoke() {
@@ -24,4 +26,43 @@ function makeJoke() {
             .then((res) => res.json());
         return request;
     });
+}
+function pushJoke(joke) {
+    if (jokes.length == 0) {
+        let sendJoke = new JOKE(joke.id, joke.joke, joke.status);
+        jokes.push(sendJoke); // push the array
+    }
+    else {
+        let check = false;
+        jokes.forEach(pro => {
+            if (pro.getId == joke.id) {
+                check = true;
+            }
+        });
+        if (check == false) {
+            let sendJoke = new JOKE(joke.id, joke.joke, joke.status);
+            jokes.push(sendJoke); // if the joke doesn't exist push at the array
+        }
+    }
+}
+function punctuateJokes(num) {
+    if (jokes.length == 0) {
+        alert("Upps, you don't see any joke yet");
+    }
+    else {
+        const textUser = document.getElementById("joke");
+        let text = textUser.innerHTML;
+        const date = new Date();
+        const dateISO = date.toISOString();
+        jokes.forEach(joke => {
+            if (joke.getJoke == text) {
+                const score = {
+                    Score: num,
+                    Date: dateISO,
+                };
+                joke.setScores = score;
+            }
+        });
+    }
+    console.log(jokes);
 }
--- void.ts
+++ void.ts
@@ -1,15 +1,18 @@
 const APIJOKE = 'https://icanhazdadjoke.com';
 
+//Legend: Joke = interface
+//        JOKE = Class
+//        joke = variable with interface Joke for take info
+//        jokes= array of JOKE objects (plus: inside the object save the score inside other array)
+
 interface Joke{
-    id?     : string;
-    joke    : string;
-    status? : number;
-    score?  : number;
+    id     : string;
+    joke   : string;
+    status : number;
 }
 
-const jokes : Joke[] = [];
 
-
+const jokes : JOKE[] = [];
 
 async function letsJoke(){
     
@@ -17,8 +20,10 @@ async function letsJoke(){
     const stringJoke : string = joke.joke;
     const textUser:HTMLBodyElement = document.getElementById("joke") as HTMLBodyElement;
     textUser.innerHTML= stringJoke;
-      
-} 
+
+    pushJoke(joke);
+    console.log(jokes)
+}
 
 async function makeJoke() : Promise<Joke> {
 
@@ -26,6 +31,62 @@ async function makeJoke() : Promise<Joke> {
     .then((res) => res.json())
              
     return request;
+}
+
+function pushJoke(joke : Joke){
+    
+    if(jokes.length==0){
+       
+        let sendJoke = new JOKE(joke.id,joke.joke,joke.status); 
+        jokes.push(sendJoke); // push the array
+    } else{
+        
+        let check : boolean = false;
+        jokes.forEach( pro =>{
+            if(pro.getId == joke.id){
+              
+                check = true;  
+            }
+        });
+        
+        if(check == false){
+            
+            let sendJoke = new JOKE(joke.id,joke.joke,joke.status); 
+            jokes.push(sendJoke);  // if the joke doesn't exist push at the array
+        }
+    }
+}
+
+function  punctuateJokes(num:number){
+   
+    if(jokes.length== 0){
+        alert("Upps, you don't see any joke yet");
+    
+    }else{
+
+        const textUser:HTMLBodyElement = document.getElementById("joke") as HTMLBodyElement;
+        let text :string = textUser.innerHTML;
+        
+        const date : Date = new Date()
+        const dateISO : string = date.toISOString()
+
+        jokes.forEach( joke =>{
+            
+            if(joke.getJoke == text){
+                
+                const score : Score = { 
+                    Score: num ,
+                    Date : dateISO,
+                }
+                
+                joke.setScores = score;
+                
+            }
+        })
+
+    }
+
+    console.log (jokes)
 }
 
 
